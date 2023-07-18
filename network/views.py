fc1763e032550f26efe3d98fe5d0877b6e1243a6
Roma Khajiev <raxmon1710@gmail.com>
2023-07-19 02:37:32 +0800

    Just finished the registration form. Picture field should be get from django next.

---
+++ network/views.py
@@ -0,0 +1,78 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+
+from .models import User, Post
+
+
+def index(request):
+    return render(request, "network/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "network/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "network/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "network/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "network/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/register.html")
+
+def posts(request):
+    # Save the post when a form is submitted
+    if request.POST:
+        title = request.POST['title']
+        content = request.POST['content']
+        user = request.user
+        post = Post.objects.create(author=user,title=title,content=content)
+        post.save()
+        all_posts = Post.objects.order_by('-created_at')
+        return render(request, "network/posts.html", {"posts":all_posts, "message": "Your post has been added sucessfully."})
+
+    all_posts = Post.objects.order_by('-created_at')
+    return render(request, "network/posts.html", {"posts":all_posts})
