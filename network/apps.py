5e786e7d3ccac72cb36ad782740e9bba930424ce
Roma Khajiev <raxmon1710@gmail.com>
2023-08-27 00:36:29 +0800

    Your commit message

--- network/apps.py
+++ network/apps.py
@@ -3,7 +3,3 @@ from django.apps import AppConfig
 
 class NetworkConfig(AppConfig):
     name = 'network'
-
-    def ready(self):
-        # Run your custom command
-        RunTailwindCommand().handle()
